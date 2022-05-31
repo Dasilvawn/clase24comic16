9521fac54c7c7f600850469b4dc4521db35bb835
Walter <dasilvawn@gmail.com>
2022-05-30 22:40:50 -0300

    estructura inicial

---
+++ cajero.js
@@ -0,0 +1,69 @@
+const process = require ('process')
+
+const entradaOperacion = process.argv[2].toLowerCase();
+const entradaMonto = +process.argv[3];
+const entradaNombreUsuario = process.argv[4];
+
+const miCuenta = {
+    nombre: "Ezequiel",
+    dinero: 100000,
+    numeroCuenta: '3'
+
+}
+
+const usuarioDisponibles = [ const usuariosDisponibles = [
+    {
+      nombre: "Alberto",
+      dinero: 0,
+      numeroCuenta: "1",
+    },
+    {
+      nombre: "Cristian",
+      dinero: 0,
+      numeroCuenta: "2",
+    },
+    {
+      nombre: "Maximiliano",
+      dinero: 0,
+      numeroCuenta: "4",
+    },
+    {
+      nombre: "Fabian",
+      dinero: 0,
+      numeroCuenta: "5",
+    },
+  ];
+
+];
+ 
+function esUnaAccionValida(){
+
+}
+
+function operacionEgreso(miUsuario, monto) {
+miUsuario.dinero = miUsuario.dinero - monto;
+}
+
+function operacionIngreso(miUsuario, monto) {
+    miUsuario.dinero = miUsuario.dinero + monto;
+    }
+
+function usuarioDestino(entradaUsuarioDestino, usuarioDisponibles){
+    const
+}    
+
+//funcion madre
+    function cajeroAutomatico(usuarioActual, monto, operacion){
+
+   if (esUnaOperacionValida(operacion)){
+
+    if (operacion === "extraccion" || operacion == "extraer"){
+        operacionEgreso(usuarioActual, monto)
+    }
+
+   }else{
+       return "La operacion ingresada es invalida"
+   }
+    }
+    
+    cajeroAutomatico(miCuenta)
---
+++ condicional.js
@@ -0,0 +1,10 @@
+function puedePasar (nombre, apellido){
+
+    if (nombre === "Cosme" && apellido === "Fulanito"){
+        return false
+    }else {
+     return true
+    }
+}
+console.log(puedePasar ("maria", "maria"));
+console.log(puedePasar ("Cosme", "Fulanito"));
